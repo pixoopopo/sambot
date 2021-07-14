5f4c9c9f28cb8f28536a4762e32af69f4c37647a
pixoopopo <86321007+pixoopopo@users.noreply.github.com>
2021-07-14 15:24:36 +0200

    Add files via upload

---
+++ Cogs/musiccog.py
@@ -0,0 +1,127 @@
+import discord
+from discord.ext import commands
+
+from youtube_dl import YoutubeDL
+import validators
+
+class music_cog(commands.Cog):
+    def __init__(self, app):
+        self.app = app
+    
+        #all the music related stuff
+        self.is_playing = False
+
+        # 2d array containing [song, channel]
+        self.music_queue = []
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        self.vc = ""
+
+     #searching the item on youtube
+    def search_yt(self, item):
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            try: 
+                info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
+            except Exception: 
+                return False
+
+        return {'source': info['formats'][0]['url'], 'title': info['title']}
+
+    def search_url(self, url):
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            try:
+                info = ydl.extract_info(url, download=False)
+                print(info)
+                URL ={'source' : info['formats'][0]['url'], 'title': info['title']}
+                print(URL)
+            except Exception: 
+                return False
+        return URL
+    def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            #get the first url
+            m_url = self.music_queue[0][0]['source']
+
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    # infinite loop checking 
+    async def play_music(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            m_url = self.music_queue[0][0]['source']
+            
+            #try to connect to voice channel if you are not already connected
+
+            if self.vc == "" or not self.vc.is_connected() or self.vc == None:
+                self.vc = await self.music_queue[0][1].connect()
+            else:
+                await self.vc.move_to(self.music_queue[0][1])
+            
+            print(self.music_queue)
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    @commands.command(name="p", help="Plays a selected song from youtube")
+    async def p(self, ctx, *args):
+        query = " ".join(args)
+        print (query)
+        #valid = validators.url(query)
+        voice_channel = ctx.author.voice.channel
+        if voice_channel is None:
+            #you need to be connected so that the bot knows where to go
+            await ctx.send("Connect to a voice channel!")
+        else:
+            song = self.search_yt(query)
+            url1= self.search_url(query)
+            
+
+            if query.startswith('htt'):
+                
+                await ctx.send(f"노래 추가 됨: {url1['title']}")
+                self.music_queue.append([url1, voice_channel])
+                
+                if self.is_playing == False:
+                    await self.play_music()
+
+            else:
+                await ctx.send(f"추가된 노래: {song['title']}")
+                self.music_queue.append([song, voice_channel])
+                
+                if self.is_playing == False:
+                    await self.play_music()
+
+    @commands.command(name="q", help="Displays the current songs in queue")
+    async def q(self, ctx):
+        retval = ""
+        for i in range(0, len(self.music_queue)):
+            retval += self.music_queue[i][0]['title'] + "\n"
+
+        print(retval)
+        await ctx.send('다음 재생항목:')
+        if retval != "":
+            await ctx.send(retval)
+        else:
+            await ctx.send("다음 재생 항목이 없습니다.")
+
+    @commands.command(name="skip", help="Skips the current song being played")
+    async def skip(self, ctx):
+        if self.vc != "" and self.vc:
+            self.vc.stop()
+            #try to play next in the queue if it exists
+            await self.play_music()
+
+def setup(app):
+    app.add_cog(music_cog(app))
